b867d4c7a963a9bf68bbbfb6e898ccf7fceee621
Eugene Epishev <eepishev1989@gmail.com>
2018-11-16 11:47:52 +0200

    Update searchTwinfield.spec.js

--- test/searchTwinfield.spec.js
+++ test/searchTwinfield.spec.js
@@ -1,20 +1,18 @@
 const GooglePage = require('./pageObjects/google.pageObject.js');
 
 describe('Twinfield Demo Tests', function () {
-
+let google = new GooglePage();
     beforeEach(async function () {
         browser.ignoreSynchronization = true;
         browser.get('https://www.google.com');
     });
 
     it('Twinfield should be first in google search', async function () {
-        let google = new GooglePage();
         await google.search('Twinfield');
         expect(google.getFirstResultLink()).toEqual('https://www.twinfield.co.uk/');
     });
 
     it('Open link from search results which contains word \'Twinfield\'', async function () {
-        let google = new GooglePage();
         await google.search('Twinfield');
         let availableLinks = await google.getLinkThatContains('Twinfield');
         availableLinks.forEach(function (item) {
